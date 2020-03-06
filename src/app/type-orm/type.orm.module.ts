96989a78955338457a98ef1f6d49a2902a39d81d
Nikita <kni2012@ukr.net>
2020-03-06 19:42:01 +0200

    fix db

--- src/app/type-orm/type.orm.module.ts
+++ src/app/type-orm/type.orm.module.ts
@@ -5,12 +5,12 @@ import { TypeOrmModule as NestTypeOrmModule } from '@nestjs/typeorm'
 const modules = [
   NestTypeOrmModule.forRoot({
     type: 'postgres',
-    host: "localhost",
+    host: "ec2-35-168-54-239.compute-1.amazonaws.com",
     port: 5432,
     synchronize: true,
-    username: "postgres",
-    password: "admin",
-    database: "artistsdb",
+    username: "kjjqcpsnxmeaka",
+    password: "0b98afd94f1234c89d1b8188ac9a1d4a1bdd6811b222d3212d520b6a61b53a27",
+    database: "d5lj29t5f366v9",
     entities: [__dirname + '../../../**/*.entity.{ts,js}', __dirname + '../../../shared/entities/*.entity.{ts,js}']
   })
 ] 
